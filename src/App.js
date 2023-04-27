400fe86a622418f906aa709895be37e1a0a745ef
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-27 18:21:16 +0430

    Adding a Meals Componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import   {Fragment} from "react";
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
        <Header/>
+       <main> 
+          <Meals />
+       </main>
     </Fragment>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
