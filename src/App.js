f758583cdb34033d3d6833e0aee841239e8ae054
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-28 15:58:35 +0430

    managing the context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import   {Fragment , useState} from "react";
+import   { useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from './components/Cart/Cart'
-
+import CartProvider from "./store/CartProvider";
 
 function App() {
 
@@ -16,13 +16,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
        <Header onShowCart = {showCartHandler} />
        <main> 
           <Meals />
        </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
+
+    const cartCtx =  useContext(CartContext)
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber , item) =>{
+        return curNumber + item.amount
+    } , 0) ;
+
     return(
         <button className={classes.button } onClick={props.onClick}>
             <span className={classes.icon}>
@@ -9,7 +18,7 @@ const HeaderCartButton = props => {
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
