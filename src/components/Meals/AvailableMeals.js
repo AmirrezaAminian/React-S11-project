647121bdc298f7036b8969764932443c20e8156f
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-30 13:22:31 +0430

    Working with Refs & Forwarding Refs

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,6 +32,7 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
