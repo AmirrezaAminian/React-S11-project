e6e554851b66b22fc71f694e30999564015d4ce2
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-18 18:51:55 +0430

    Working with Refs & Forward Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,33 @@
-import classes from './MealItem.module.css'
-import MealsItemForm from './MealItemForm';
+import { useContext } from "react";
+import classes from "./MealItem.module.css";
+import MealsItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
-const MealItem = props => {
-    const price = `$${props.price}`
-    return (
-        <li className={classes.meal}>
-            <div>
-                <h3>{props.name}</h3>
-                <div className={classes.description}>{props.description}</div>
-                <div className={classes.price}>{price}</div>
-            </div>
-            <div>
-                <MealsItemForm />
-            </div>
-        </li>
-    )
-} ;
+const MealItem = (props) => {
+   const cartCtx =  useContext(CartContext)
+  const price = `$${props.price}`;
 
-export default MealItem ;
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+        id : props.id , 
+        name : props.name,
+        amount : amount ,
+        price : props.price,
+    })
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealsItemForm onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,35 @@
+import { useRef, useState } from "react";
+
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/input";
 
 const MealsItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false)
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
+        ref={amountInputRef}
         input={{
           id: "amount",
           type: "number",
@@ -16,6 +40,7 @@ const MealsItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
