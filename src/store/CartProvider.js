ffa5b16bfb3620883f9b9b243fd060da42bbd4d8
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-20 18:53:35 +0430

    Working with Refs & Forward Refs

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,7 +5,7 @@ import CartContext from "./cart-context";
 const defaultCartState = {
     items : [],
     totalAmount : 0
-}
+} 
 
 const cartReducer = (state , action) => {
     if(action.type === 'ADD'){
