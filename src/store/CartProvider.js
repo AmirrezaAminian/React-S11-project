7c848d08a4e2fa7f7fa42b7d705ef615c9102400
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-21 14:46:57 +0430

    Making Items Removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -32,11 +32,35 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
+    
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if(action.type === 'REMOVE'){
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id 
+    )
+    const existingItem = state.items[existingCartItemIndex]
+    const updatedTotalAmount = state.totalAmount - existingItem.price ;
+    let updatedItems ;
+    if(existingItem.amount === 1){
+      updatedItems = state.items.filter(item => item.id !== action.id );
+    }else{
+        const updatedItem = {...existingItem , amount: existingItem.amount - 1}
+        updatedItems = [...state.items] ;
+        updatedItems[existingCartItemIndex] =  updatedItem
+    }
+
+    return {
+      items : updatedItems ,
+      totalAmount : updatedTotalAmount
+    }
+  }
+
   return defaultCartState;
 };
 
